14e161fccce9b87be09d0f75f05639195f65539a
Arif Rahman <arifbb79@gmail.com>
2024-04-26 20:32:55 +0600

    updated

--- functions.php
+++ functions.php
@@ -14,12 +14,19 @@
  * @since 1.0.0
  */
 
-if ( ! function_exists( 'lesson_one_setup' ) ) {
-	function lesson_one_setup() {
+if ( ! function_exists( 'blockify_setup' ) ) {
+	function blockify_setup() {
+
 		add_theme_support( 'wp-block-styles' );
+
+		add_theme_support( 'align-wide' );
+
+		add_theme_support( 'editor-styles' );
+		add_editor_style( 'style-editor.css' );
+		add_theme_support( 'responsive-embeds' );
 	}
 }
-add_action( 'after_setup_theme', 'lesson_one_setup' );
+add_action( 'after_setup_theme', 'blockify_setup' );
 
 
 function blockify_styles() {
